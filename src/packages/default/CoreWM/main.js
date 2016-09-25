deac3bb163a24d43a1366d0fffc352b2f14df4ac
Anders Evenrud <andersevenrud@gmail.com>
2016-09-25 14:50:34 +0200

    Settings: Now possible to edit hotkeys

--- src/packages/default/CoreWM/main.js
+++ src/packages/default/CoreWM/main.js
@@ -908,6 +908,8 @@
       }
     }
 
+    this.generatedHotkeyMap = {};
+
     var keys = this._settings.get('hotkeys');
     Object.keys(keys).forEach(function(k) {
       self.generatedHotkeyMap[keys[k]] = function() {
